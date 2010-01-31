12ce56a3916c03b8419a2b4154c91187f0fa3982
Pascal Chambon <pythoniks@gmail.com>
2010-01-31 23:00:48 +0100

    Beginning the cleaning of everything, for first releases

--- rstest/__launch.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import os
-
-path = os.path.join(os.path.dirname(__file__), "__inheritance_tester.py")
-args = (path,)
-print "--->", path, "-", args
-retcode = os.spawnvp(os.P_WAIT, "path", "__inheritance_tester.py", ())
-print "=>", retcode
