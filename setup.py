81de7817dbc8cea2f83cc28d009b10a35028b79f
Pascal Chambon <chambon.pascal@gmail.com>
2016-06-08 11:48:18 +0200

    Switch from distutils to setuptools in setup.py

--- setup.py
+++ setup.py
@@ -29,31 +29,33 @@ Operating System :: MacOS :: MacOS X
 
 doclines = [line.strip() for line in __doc__.split("\n")]
 
+import sys, os
 
-import sys
-from distutils.core import setup
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-try:
-    from distutils.command.build_py import build_py_2to3 as build_py
-    print("Compiling via Py2to3")
-except ImportError:
-    from distutils.command.build_py import build_py
-    print("Normal python build")
+from setuptools import setup
 
 
-setup(name='RSFile',
-      version='1.2',
-      author='Pascal Chambon',
-      author_email='pythoniks@gmail.com',
-      url='http://bitbucket.org/pchambon/python-rock-solid-tools/',
-      license="http://www.opensource.org/licenses/mit-license.php",
-      platforms=["any"],
-      description=doclines[0],
-      classifiers=filter(None, classifiers.split("\n")),
-      long_description=" ".join(doclines[2:]),
-      # "rstest", "rstest.stdlib" TODO - can't be included atm because they use __file__, incompatible with EGGS
-      packages=("rsfile", "rsfile.rsbackend", "rsfile.rstest", "rsfile.rstest.stdlib"),
-      cmdclass={'build_py': build_py},
-      zip_safe=False,  # tests refer to "__file__", so let's be safe if we one day use setuptools...
-     )
+setup(
+    name='RSFile',
+    version='1.2',
+    author='Pascal Chambon',
+    author_email='pythoniks@gmail.com',
+    url='http://bitbucket.org/pchambon/python-rock-solid-tools/',
+    license="http://www.opensource.org/licenses/mit-license.php",
+    platforms=["any"],
+    description=doclines[0],
+    classifiers=filter(None, classifiers.split("\n")),
+    long_description=" ".join(doclines[2:]),
+
+    #package_dir={'': 'src'},
+    packages=("rsfile", "rsfile.rsbackend", "rsfile.rstest", "rsfile.rstest.stdlib"),
+
+    # test_suite='your.module.tests',
+
+    use_2to3=True,
+    #convert_2to3_doctests=['src/your/module/README.txt'],
+    #use_2to3_fixers=['your.fixers'],
+    #use_2to3_exclude_fixers=['lib2to3.fixes.fix_import'],
+)
 
