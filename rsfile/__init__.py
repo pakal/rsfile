2d5303709e9f3762658876dbb9c89c6a12af393c
Pascal Chambon <chambon.pascal@gmail.com>
2016-06-21 17:42:43 +0200

    Do some cleanup in __init__.py

--- rsfile/__init__.py
+++ rsfile/__init__.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
-import sys, os
-import functools
-
 # must be FIRST - VERY IMPORTANT !
 from .rsfile_definitions import *  # constants, base types and exceptions
 
@@ -14,9 +11,3 @@ from .rsfile_factories import *
 from .rsfile_registries import set_rsfile_options, get_rsfile_options
 from .rsfile_utilities import *
 
-
-
-
-
-
-# contract.checkmod(module)  # UNCOMMENT THIS TO ACTIVATE PYCONTRACT CHECKING
