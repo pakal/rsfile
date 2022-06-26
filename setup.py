b85fea3cdfdb354cd3e5f72458fad43a53c4c6cb
Pascal Chambon <chambon.pascal@gmail.com>
2022-06-26 13:33:39 +0200

    Update setup.py classifiers to match new supported python versions

--- setup.py
+++ setup.py
@@ -9,19 +9,24 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-classifiers = """\
-Development Status :: 5 - Production/Stable
-Intended Audience :: Developers
-Intended Audience :: Information Technology
-Intended Audience :: System Administrators
-License :: OSI Approved :: MIT License
-Programming Language :: Python
-Topic :: System :: Filesystems
-Topic :: Software Development :: Libraries :: Python Modules
-Operating System :: Microsoft :: Windows
-Operating System :: Unix
-Operating System :: MacOS :: MacOS X
-"""
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Information Technology",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Topic :: System :: Filesystems",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: Unix",
+    "Operating System :: MacOS :: MacOS X",
+]
+assert all(classifiers), classifiers
 
 
 setup(
@@ -32,18 +37,10 @@ setup(
     url='https://github.com/pakal/rsfile',
     license="http://www.opensource.org/licenses/mit-license.php",
     platforms=["any"],
-    description="RSFile advanced I/O file streams",
-    classifiers=filter(None, classifiers.split("\n")),
+    description="Advanced I/O file streams with fine-grained locking and creation options",
+    classifiers=classifiers,
     long_description=read("README.rst"),
 
-    #package_dir={'': 'src'},
     packages=("rsfile", "rsfile.rsbackend", "rsfile.rstest", "rsfile.rstest.stdlib"),
-
-    # test_suite='your.module.tests',
-
-    use_2to3=True,
-    #convert_2to3_doctests=['src/your/module/README.txt'],
-    #use_2to3_fixers=['your.fixers'],
-    #use_2to3_exclude_fixers=['lib2to3.fixes.fix_import'],
 )
 
